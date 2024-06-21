81ec398823ea14385966d77e026316350b23daa2
Matthew Clarke <m_d_clarke@hotmail.com>
2024-06-21 12:56:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to connect and sync Bryntum Gantt to ServiceNow Project Portfolio Management planning chart: starter repository
+
+The code for the completed app is in the `completed-app` branch.
+
+Install the dependencies using the following command:
+
+```bash
+npm install
+```
---
+++ server.js
@@ -0,0 +1,18 @@
+import bodyParser from "body-parser";
+import "dotenv/config";
+import express from "express";
+import path from "path";
+
+global.__dirname = path.resolve();
+
+const port = process.env.PORT || 1337;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json());
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
